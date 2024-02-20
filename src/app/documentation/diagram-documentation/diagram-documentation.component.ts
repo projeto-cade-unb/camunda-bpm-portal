e00de7266afd4cf72b5456b45fcc61fbd7b0a987
sandrewtx08 <sandrewtx08@pm.me>
2024-02-20 17:36:38 -0300

    fix: first diagram documentation element

--- src/app/documentation/diagram-documentation/diagram-documentation.component.ts
+++ src/app/documentation/diagram-documentation/diagram-documentation.component.ts
@@ -16,21 +16,22 @@ export class DiagramDocumentationComponent implements OnInit {
   ngOnInit(): void {
     const document = this.domParser.parseFromString(this.diagram, "text/xml");
 
-    const processChildrens = document.querySelectorAll("process > *");
+    const processChildren = document.querySelectorAll("process > *");
     const processContaier = document.querySelector("process");
 
     this.diagramDocumentation.push({
-      documentation: processChildrens[0]?.textContent,
+      documentation:
+        processChildren[0].tagName === "documentation" &&
+        processChildren[0]?.textContent,
       extendedDocumentation: processContaier.getAttribute(
         "documentation:extendedDocumentation"
       ),
     });
 
-    processChildrens.forEach((processElement) => {
+    processChildren.forEach((processChild) => {
       this.diagramDocumentation.push({
-        documentation:
-          processElement.querySelector("documentation")?.textContent,
-        extendedDocumentation: processElement.getAttribute(
+        documentation: processChild.querySelector("documentation")?.textContent,
+        extendedDocumentation: processChild.getAttribute(
           "documentation:extendedDocumentation"
         ),
       });
