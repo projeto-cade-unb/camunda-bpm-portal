b42d380fa125c2c23a17c2907bd7ed5fb1f58bbb
Your Name <sandrewtx08@pm.me>
2024-09-02 11:00:58 -0300

    feat: user permission view definition

--- src/main/java/com/ambientelivre/plugin/service/ProcessDefinitionDocumentationService.java
+++ src/main/java/com/ambientelivre/plugin/service/ProcessDefinitionDocumentationService.java
@@ -25,188 +25,192 @@ import com.ambientelivre.plugin.ProcessDefinitionDocumentationElement;
 import com.ambientelivre.plugin.utils.BpmnXmlNamespaceUri;
 
 public class ProcessDefinitionDocumentationService extends AbstractCockpitPluginResource {
-    public ProcessDefinitionDocumentationService(String engineName) {
-        super(engineName);
-    }
+        public ProcessDefinitionDocumentationService(String engineName) {
+                super(engineName);
+        }
 
-    public List<ProcessDefinitionDocumentation> findManyProcessDefinitionDocumentation() {
-        return processDefinitionsToDocumentation(getProcessEngine()
-                .getRepositoryService()
-                .createProcessDefinitionQuery()
-                .latestVersion()
-                .list());
-    }
+        public List<ProcessDefinitionDocumentation> findManyProcessDefinitionDocumentation() {
+                return processDefinitionsToDocumentation(getProcessEngine()
+                                .getRepositoryService()
+                                .createProcessDefinitionQuery()
+                                .latestVersion()
+                                .list());
+        }
 
-    public List<ProcessDefinitionDocumentation> processDefinitionsToDocumentation(
-            List<ProcessDefinition> processDefinitions) {
-        Authentication currentAuthentication = getProcessEngine()
-                .getIdentityService()
-                .getCurrentAuthentication();
+        public List<ProcessDefinitionDocumentation> processDefinitionsToDocumentation(
+                        List<ProcessDefinition> processDefinitions) {
+                Authentication currentAuthentication = getProcessEngine()
+                                .getIdentityService()
+                                .getCurrentAuthentication();
 
-        String currentUserId = currentAuthentication != null ? currentAuthentication.getUserId() : null;
+                String currentUserId = currentAuthentication != null ? currentAuthentication.getUserId() : null;
 
-        Boolean isAuthenticated = currentUserId != null ? !currentUserId.isBlank() : false;
+                Boolean isAuthenticated = currentUserId != null ? !currentUserId.isBlank() : false;
 
-        if (!isAuthenticated) {
-            AuthorizationQuery authorizationQuery = getProcessEngine()
-                    .getAuthorizationService()
-                    .createAuthorizationQuery()
-                    .resourceType(6);
+                if (!isAuthenticated) {
+                        AuthorizationQuery authorizationQuery = getProcessEngine()
+                                        .getAuthorizationService()
+                                        .createAuthorizationQuery()
+                                        .resourceType(6);
 
-            Permission[] requiredPermissions = new Permission[] {
-                    Permissions.READ,
-                    Permissions.READ_HISTORY
-            };
+                        Permission[] requiredPermissions = new Permission[] {
+                                        Permissions.READ,
+                                        Permissions.READ_HISTORY
+                        };
 
-            List<String> authorizedResourceIds = authorizationQuery
-                    .list()
-                    .stream()
-                    .map(currentAuthorization -> {
-                        Permission[] grantedPermissions = currentAuthorization.getPermissions(requiredPermissions);
+                        List<String> authorizedResourceIds = authorizationQuery
+                                        .list()
+                                        .stream()
+                                        .map(currentAuthorization -> {
+                                                Permission[] grantedPermissions = currentAuthorization
+                                                                .getPermissions(requiredPermissions);
 
-                        if (currentAuthorization.getAuthorizationType() == Authorization.AUTH_TYPE_GLOBAL
-                                && grantedPermissions.length > 0
-                                && currentAuthorization.getUserId().equals("*")) {
-                            return currentAuthorization.getResourceId();
+                                                if (currentAuthorization
+                                                                .getAuthorizationType() == Authorization.AUTH_TYPE_GLOBAL
+                                                                && grantedPermissions.length > 0
+                                                                && (currentAuthorization.getUserId().equals("*")
+                                                                                || currentAuthorization.getGroupId()
+                                                                                                .equals("*"))) {
+                                                        return currentAuthorization.getResourceId();
+                                                }
+
+                                                return null;
+                                        })
+                                        .filter(value -> value != null)
+                                        .collect(Collectors.toList());
+
+                        if (authorizedResourceIds.size() > 0) {
+                                return getProcessEngine()
+                                                .getRepositoryService()
+                                                .createProcessDefinitionQuery()
+                                                .latestVersion()
+                                                .processDefinitionKeysIn(authorizedResourceIds.toArray(new String[0]))
+                                                .list()
+                                                .stream()
+                                                .map(this::createProcessDefinitionDocumentation)
+                                                .collect(Collectors.toList());
                         }
 
-                        return null;
-                    })
-                    .filter(value -> value != null)
-                    .collect(Collectors.toList());
+                        return List.of();
+                }
 
-            if (authorizedResourceIds.size() > 0) {
                 return getProcessEngine()
-                        .getRepositoryService()
-                        .createProcessDefinitionQuery()
-                        .latestVersion()
-                        .processDefinitionKeysIn(authorizedResourceIds.toArray(new String[0]))
-                        .list()
-                        .stream()
-                        .map(this::createProcessDefinitionDocumentation)
-                        .collect(Collectors.toList());
-            }
-
-            return List.of();
+                                .getRepositoryService()
+                                .createProcessDefinitionQuery()
+                                .latestVersion()
+                                .startablePermissionCheck()
+                                .list()
+                                .stream()
+                                .map(this::createProcessDefinitionDocumentation)
+                                .collect(Collectors.toList());
         }
 
-        // TODO: check other user permissions
-        return getProcessEngine()
-                .getRepositoryService()
-                .createProcessDefinitionQuery()
-                .latestVersion()
-                .list()
-                .stream()
-                .map(this::createProcessDefinitionDocumentation)
-                .collect(Collectors.toList());
-    }
-
-    public ProcessDefinitionDocumentation findOneProcessDefinitionDocumentation(
-            String processDefinitionKey) {
-        return processDefinitionsToDocumentation(getProcessEngine()
-                .getRepositoryService()
-                .createProcessDefinitionQuery()
-                .processDefinitionKey(processDefinitionKey)
-                .latestVersion()
-                .list())
-                .get(0);
-
-    }
-
-    private ProcessDefinitionDocumentation createProcessDefinitionDocumentation(ProcessDefinition processDefinition) {
-        InputStream bpmnXmlFile = getProcessEngine()
-                .getRepositoryService()
-                .getProcessModel(processDefinition.getId());
-        BpmnModelInstance modelInstance = Bpmn.readModelFromStream(bpmnXmlFile);
-
-        List<ProcessDefinitionDocumentationElement> documentationList = extractDocumentation(modelInstance);
-        String bpmnXmlText = Bpmn.convertToString(modelInstance);
-
-        try {
-            bpmnXmlFile.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        public ProcessDefinitionDocumentation findOneProcessDefinitionDocumentation(
+                        String processDefinitionKey) {
+                return processDefinitionsToDocumentation(getProcessEngine()
+                                .getRepositoryService()
+                                .createProcessDefinitionQuery()
+                                .processDefinitionKey(processDefinitionKey)
+                                .latestVersion()
+                                .listPage(0, 1))
+                                .get(0);
         }
 
-        return new ProcessDefinitionDocumentation(
-                processDefinition.getKey(),
-                processDefinition.getName(),
-                bpmnXmlText,
-                documentationList);
-    }
+        private ProcessDefinitionDocumentation createProcessDefinitionDocumentation(
+                        ProcessDefinition processDefinition) {
+                InputStream bpmnXmlFile = getProcessEngine()
+                                .getRepositoryService()
+                                .getProcessModel(processDefinition.getId());
+                BpmnModelInstance modelInstance = Bpmn.readModelFromStream(bpmnXmlFile);
 
-    private List<ProcessDefinitionDocumentationElement> extractDocumentation(BpmnModelInstance modelInstance) {
-        List<ProcessDefinitionDocumentationElement> documentationList = new ArrayList<>();
+                List<ProcessDefinitionDocumentationElement> documentationList = extractDocumentation(modelInstance);
+                String bpmnXmlText = Bpmn.convertToString(modelInstance);
 
-        ProcessDefinitionDocumentationElement processDoc = modelInstance
-                .getModelElementsByType(Process.class)
-                .stream()
-                .map(this::createDocumentation)
-                .filter(value -> value != null)
-                .findFirst()
-                .orElse(null);
+                try {
+                        bpmnXmlFile.close();
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
 
-        if (processDoc != null) {
-            documentationList.add(processDoc);
+                return new ProcessDefinitionDocumentation(
+                                processDefinition.getKey(),
+                                processDefinition.getName(),
+                                bpmnXmlText,
+                                documentationList);
         }
 
-        documentationList.addAll(
-                modelInstance
-                        .getModelElementsByType(FlowNode.class)
-                        .stream()
-                        .map(this::createDocumentation)
-                        .filter(value -> value != null)
-                        .collect(Collectors.toList()));
+        private List<ProcessDefinitionDocumentationElement> extractDocumentation(BpmnModelInstance modelInstance) {
+                List<ProcessDefinitionDocumentationElement> documentationList = new ArrayList<>();
 
-        return documentationList;
-    }
+                ProcessDefinitionDocumentationElement processDoc = modelInstance
+                                .getModelElementsByType(Process.class)
+                                .stream()
+                                .map(this::createDocumentation)
+                                .filter(value -> value != null)
+                                .findFirst()
+                                .orElse(null);
 
-    private ProcessDefinitionDocumentationElement createDocumentation(FlowNode flowNode) {
-        String documentation = getDocumentation(flowNode);
-        String extendedDocumentation = flowNode.getAttributeValueNs(BpmnXmlNamespaceUri.DOCUMENTATION,
-                "extendedDocumentation");
+                if (processDoc != null) {
+                        documentationList.add(processDoc);
+                }
 
-        if ((documentation == null || documentation.isBlank()) && (extendedDocumentation == null
-                || extendedDocumentation.isBlank())) {
-            return null;
+                documentationList.addAll(
+                                modelInstance
+                                                .getModelElementsByType(FlowNode.class)
+                                                .stream()
+                                                .map(this::createDocumentation)
+                                                .filter(value -> value != null)
+                                                .collect(Collectors.toList()));
+
+                return documentationList;
         }
 
-        return new ProcessDefinitionDocumentationElement(
-                flowNode.getId(),
-                flowNode.getName(),
-                flowNode.getAttributeValueNs(BpmnXmlNamespaceUri.CAMUNDA, "assignee"),
-                flowNode.getAttributeValueNs(BpmnXmlNamespaceUri.CAMUNDA, "candidateGroups"),
-                flowNode.getAttributeValueNs(BpmnXmlNamespaceUri.CAMUNDA, "dueDate"),
-                documentation,
-                extendedDocumentation);
-    }
+        private ProcessDefinitionDocumentationElement createDocumentation(FlowNode flowNode) {
+                String documentation = getDocumentation(flowNode);
+                String extendedDocumentation = flowNode.getAttributeValueNs(BpmnXmlNamespaceUri.DOCUMENTATION,
+                                "extendedDocumentation");
 
-    // TODO: fix duplicated method
-    private ProcessDefinitionDocumentationElement createDocumentation(Process process) {
-        String documentation = getDocumentation(process);
-        String extendedDocumentation = process.getAttributeValueNs(BpmnXmlNamespaceUri.DOCUMENTATION,
-                "extendedDocumentation");
+                if ((documentation == null || documentation.isBlank()) && (extendedDocumentation == null
+                                || extendedDocumentation.isBlank())) {
+                        return null;
+                }
 
-        if ((documentation == null || documentation.isBlank()) && (extendedDocumentation == null
-                || extendedDocumentation.isBlank())) {
-            return null;
+                return new ProcessDefinitionDocumentationElement(
+                                flowNode.getId(),
+                                flowNode.getName(),
+                                flowNode.getAttributeValueNs(BpmnXmlNamespaceUri.CAMUNDA, "assignee"),
+                                flowNode.getAttributeValueNs(BpmnXmlNamespaceUri.CAMUNDA, "candidateGroups"),
+                                flowNode.getAttributeValueNs(BpmnXmlNamespaceUri.CAMUNDA, "dueDate"),
+                                documentation,
+                                extendedDocumentation);
         }
 
-        return new ProcessDefinitionDocumentationElement(
-                process.getId(),
-                process.getName(),
-                process.getAttributeValueNs(BpmnXmlNamespaceUri.CAMUNDA, "assignee"),
-                process.getAttributeValueNs(BpmnXmlNamespaceUri.CAMUNDA, "candidateGroups"),
-                process.getAttributeValueNs(BpmnXmlNamespaceUri.CAMUNDA, "dueDate"),
-                documentation,
-                extendedDocumentation);
-    }
+        // TODO: fix duplicated method
+        private ProcessDefinitionDocumentationElement createDocumentation(Process process) {
+                String documentation = getDocumentation(process);
+                String extendedDocumentation = process.getAttributeValueNs(BpmnXmlNamespaceUri.DOCUMENTATION,
+                                "extendedDocumentation");
 
-    private String getDocumentation(BaseElement baseElement) {
-        Documentation doc = baseElement.getDocumentations()
-                .stream()
-                .findFirst()
-                .orElse(null);
-        return doc != null ? doc.getTextContent() : "";
-    }
+                if ((documentation == null || documentation.isBlank()) && (extendedDocumentation == null
+                                || extendedDocumentation.isBlank())) {
+                        return null;
+                }
+
+                return new ProcessDefinitionDocumentationElement(
+                                process.getId(),
+                                process.getName(),
+                                process.getAttributeValueNs(BpmnXmlNamespaceUri.CAMUNDA, "assignee"),
+                                process.getAttributeValueNs(BpmnXmlNamespaceUri.CAMUNDA, "candidateGroups"),
+                                process.getAttributeValueNs(BpmnXmlNamespaceUri.CAMUNDA, "dueDate"),
+                                documentation,
+                                extendedDocumentation);
+        }
+
+        private String getDocumentation(BaseElement baseElement) {
+                Documentation doc = baseElement.getDocumentations()
+                                .stream()
+                                .findFirst()
+                                .orElse(null);
+                return doc != null ? doc.getTextContent() : "";
+        }
 }
